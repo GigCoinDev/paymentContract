43670b5fbe7cc7a91c7a5c7ac62977126ee39b1b
hardikthecoder <85208662+hardiktheprogrammer@users.noreply.github.com>
2023-06-17 19:35:55 +0530

    paymentContract

---
+++ paymentSplitter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.16;
+
+
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+
+contract paymentSplitter is Payment {
+    constructor() payable  Payment()
+    
+    }
